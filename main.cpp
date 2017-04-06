875e6e7d26ce59854c5dbfe0204df211fe135895
mmodzel2 <mmodzel2@lab09.elka.pw.edu.pl>
2017-04-06 21:53:45 +0200

    0.2 Changes in mul and add functions. Started writting determination function

--- main.cpp
+++ main.cpp
@@ -2,22 +2,64 @@
 
 #include "matrix.hpp"
 
+#include <ctime>
+#include <cstdlib>
+
 using namespace std;
 
 int main()
 {
+    srand(time(NULL));
     Matrix <int> *m = new Matrix <int> (2,2);
-    cout << m->get(0,1) << endl;
-    cout << m->set(0,1,90) << endl;
-    cout << m->get(0,1) << endl;
-
     Matrix <int> *m1 = new Matrix <int> (2,2);
+    Matrix <int> *m2 = new Matrix <int> (4,4);
+    cout << m->get(0,0) << endl;
     cout << m->get(0,1) << endl;
+    cout << m->get(1,0) << endl;
+    cout << m->get(1,1) << endl;
     cout << m->set(0,1,90) << endl;
     cout << m->get(0,1) << endl;
 
-    Matrix <int> m2 = (*m+*m1);
-    cout << m2.get(0,1) << endl;
+    cout << m1->get(0,0) << endl;
+    cout << m1->get(0,1) << endl;
+    cout << m1->get(1,0) << endl;
+    cout << m1->get(1,1) << endl;
+    cout << m1->set(0,1,1) << endl;
+    cout << m1->set(1,1,1) << endl;
+    cout << m1->get(0,1) << endl;
+
+    for (int i = 0; i < 4; i++){
+        //for (int j = 0; j < 3; j++) cout << m2->set(i,j,i+j) << endl;
+        //cout << m2->set(i,2,i) << endl;
+        //cout << m2->set(i,0,0) << endl;
+        cout << m2->set(i,i,1) << endl;
+    }
+    cout << m->set(0,0,1) << endl;
+    cout << m->set(0,0,1) << endl;
+
+
+    /**m+=*m1;
+    cout << "Sum: " << m->get(0,1) << endl;
+    *m+=*m1;
+    *m+=*m1;
+    *m+=*m1;
+    *m+=*m1;
+    *m+=*m1;
+    cout << "Sum: " << m->get(0,1) << endl;
+
+    Matrix <int> m2 = *m+*m1;
+    cout << "Sum: " << m2.get(0,1) << endl;*/
+
+    Matrix <int> m3 = *m**m1;
+    cout << "Mul: " << m3.get(0,0) << endl;
+    cout << "Mul: " << m3.get(0,1) << endl;
+    cout << "Mul: " << m3.get(1,0) << endl;
+    cout << "Mul: " << m3.get(1,1) << endl;
+
+    cout << m3.det() << endl;
+    cout << "1111111111111111111111111111111111111" << endl;
+    cout << m2->det() << endl;
 
     delete m;
+    delete m1;
 }
--- matrix.hpp
+++ matrix.hpp
@@ -1,6 +1,9 @@
 #ifndef _MATRIX_HPP
 #define _MATRIX_HPP
 
+#include <algorithm>
+#include <iostream>
+
 template <class type>
     class Matrix{
         private:
@@ -19,10 +22,13 @@ template <class type>
             type get(unsigned long row, unsigned long column) const;
 
             Matrix<type> operator+(const Matrix<type> &m) const;
-            Matrix<type> &operator+=(const Matrix<type> &m) const;
+            Matrix<type> &operator+=(const Matrix<type> &m);
             Matrix<type> operator-(const Matrix<type> &m) const;
-            Matrix<type> &operator-=(const Matrix<type> &m) const;
+            Matrix<type> &operator-=(const Matrix<type> &m);
+            Matrix<type> operator*(const Matrix<type> &m) const;
+            Matrix<type> &operator*=(const Matrix<type> &m);
 
+            long int det(char* n = nullptr) const;
     };
 
 template <class type>
@@ -36,14 +42,22 @@ template <class type>
     Matrix <type>::Matrix(unsigned long rows, unsigned long columns){
         rows_ = rows;
         columns_ = columns;
-        if (rows == 0 && columns == 0)
+        if (rows == 0 || columns == 0)
             coefficients_ = nullptr;
-        else
-            coefficients_ = new type[rows*columns] {};
+        else {
+            coefficients_ = new type[rows*columns];
+
+            for (unsigned long i = 0; i < rows; i++){
+                for (unsigned long j = 0; j < columns; j++){
+                    coefficients_[i*columns+j] = type(0);
+                }
+            }
+            //std::fill(coefficients_, coefficients_+rows*columns*sizeof(type), type(0));
+        }
     }
 template <class type>
     Matrix <type>::~Matrix(){
-        if (coefficients_ != nullptr) delete[] coefficients_;
+        //if (coefficients_ != nullptr) delete[] coefficients_;
     }
 
 template <class type>
@@ -59,14 +73,14 @@ template <class type>
 template <class type>
     int Matrix<type>::set(unsigned long row, unsigned long column, type value){
         if (row >= rows_ || column >= columns_) return 1; //setting value beyond matrix
-        coefficients_[(row*columns_+column)*sizeof(type)] = value;
+        coefficients_[row*columns_+column] = value;
         return 0;
     }
 
 template <class type>
     type Matrix<type>::get(unsigned long row, unsigned long column) const{
         if (row >= rows_ || column >= columns_) return type(0); //getting value beyond matrix
-        return coefficients_[(row*columns_+column)*sizeof(type)];
+        return coefficients_[(row*columns_)+column];
     }
 
 template <class type>
@@ -83,8 +97,7 @@ template <class type>
     }
 
 template <class type>
-    Matrix<type> &Matrix<type>::operator+=(const Matrix<type> &m) const{
-        Matrix<type> *temp = new Matrix <type> (this->rows_, this->columns_);
+    Matrix<type> &Matrix<type>::operator+=(const Matrix<type> &m){
         if (this->rows_ == m.get_rows() && this->columns_ == m.get_columns()){
             for (unsigned long i = 0; i < this->rows_; i++){
                 for (unsigned long j = 0; j < this->columns_; j++){
@@ -109,8 +122,7 @@ template <class type>
     }
 
 template <class type>
-    Matrix<type> &Matrix<type>::operator-=(const Matrix<type> &m) const{
-        Matrix<type> *temp = new Matrix <type> (this->rows_, this->columns_);
+    Matrix<type> &Matrix<type>::operator-=(const Matrix<type> &m){
         if (this->rows_ == m.get_rows() && this->columns_ == m.get_columns()){
             for (unsigned long i = 0; i < this->rows_; i++){
                 for (unsigned long j = 0; j < this->columns_; j++){
@@ -119,6 +131,90 @@ template <class type>
             }
         }
         return *this;
+    }
+
+template <class type>
+    Matrix<type> Matrix<type>::operator*(const Matrix<type> &m) const{
+        Matrix<type> *temp;
+        if (this->columns_ != m.get_rows()) {
+                temp = new Matrix<type>;
+                return *temp; //mul is not possible
+        } else {
+            type sum;
+            temp = new Matrix <type> (this->rows_, this->columns_);
+            for (unsigned long i = 0; i < this->rows_; i++){
+                for (unsigned long j = 0; j < m.get_columns(); j++){
+                    sum = type(0);
+                    for (unsigned long k = 0; k < this->columns_; k++){
+                        sum += (this->get(i,k)*m.get(k,j));
+                    }
+                    temp->set(i, j, sum);
+                }
+            }
+            return *temp;
+        }
+    }
+
+template <class type>
+    Matrix<type> &Matrix<type>::operator*=(const Matrix<type> &m){
+        type sum;
+        if (this->columns_ == m.get_rows()) {
+            for (unsigned long i = 0; i < this->rows_; i++){
+                for (unsigned long j = 0; j < m.get_columns(); j++){
+                    sum = type(0);
+                    for (unsigned long k = 0; k < this->columns_; k++){
+                        sum += (this->get(i,k)*m.get(k,j));
+                    }
+                    this->set(i, j, sum);
+                }
+            }
+        }
+            return *this;
+    }
+
+template <class type>
+    long int Matrix<type>::det(char* n) const{
+        long int determination = 0;
+        unsigned long j = 0, k[3] = {0,0,0};
+        if (rows_ != columns_ || rows_ == 0) return 0;
+
+        if (n == nullptr){
+            n = new char[rows_];
+            std::fill(n, n+rows_*columns_*sizeof(char), 0);
+            }
+
+        for (unsigned long i = 0; i < rows_; i++)
+            if (n[i] == 0) {
+                if (j < 3) k[j] = i;
+                j++;
+                }
+
+        if (j == 3){ //matrix 3x3 detected
+            return coefficients_[(k[0]*columns_)+k[0]]*coefficients_[(k[1]*columns_)+k[1]]*coefficients_[(k[2]*columns_)+k[2]]+
+            coefficients_[(k[1]*columns_)+k[0]]*coefficients_[(k[2]*columns_)+k[1]]*coefficients_[(k[0]*columns_)+k[2]]+
+            coefficients_[(k[2]*columns_)+k[0]]*coefficients_[(k[0]*columns_)+k[1]]*coefficients_[(k[1]*columns_)+k[2]]-
+            (coefficients_[(k[0]*columns_)+k[2]]*coefficients_[(k[1]*columns_)+k[1]]*coefficients_[(k[2]*columns_)+k[0]]+
+            coefficients_[(k[1]*columns_)+k[2]]*coefficients_[(k[2]*columns_)+k[1]]*coefficients_[(k[0]*columns_)+k[0]]+
+            coefficients_[(k[2]*columns_)+k[2]]*coefficients_[(k[0]*columns_)+k[1]]*coefficients_[(k[1]*columns_)+k[0]]);
+        } else if (j == 2){ //matrix 2x2 detected
+            return coefficients_[(k[0]*columns_)+k[0]]*coefficients_[(k[1]*columns_)+k[1]]-coefficients_[(k[1]*columns_)+k[1]]*coefficients_[(k[1]*columns_)+k[0]];
+        } else if (j == 1) return k[0]; //matrix 1x1 detected
+
+        for (unsigned long i = 0; i < rows_; i++)
+            if (n[i] == 0) {
+                n[i] = 1;
+
+                for (unsigned long l = 0; l < rows_; l++){
+                    if (n[l] == 0) {
+                        if (coefficients_[(i*columns_)+l] == type(0)) continue;
+                        std::cout << i << " " << l << std::endl;
+                        n[l] = 1;
+                        if (i%2==1) determination-=coefficients_[(i*columns_)+l]*det(n);
+                        else determination=coefficients_[(i*columns_)+l]*det(n);
+                    }
+                }
+            }
+        return determination;
     }
 
 #endif
